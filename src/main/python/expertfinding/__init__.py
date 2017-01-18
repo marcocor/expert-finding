6d37631683c5b5b717a320df426c3084b1600b4f
Marco Cornolti <marco@macor.di.unipi.it>
2017-01-18 12:19:17 +0100

    Removed unnecessary data from ExpertFinding, fixed stats

--- src/main/python/expertfinding/__init__.py
+++ src/main/python/expertfinding/__init__.py
@@ -42,9 +42,6 @@ def set_cache(cache_dir):
 class ExpertFinding(object):
 
     def __init__(self, storage_db, erase=True):
-        self.papers_count = Counter()
-        self.abstract_count = Counter()
-        self.id_to_entities = dict()
         if erase and os.path.isfile(storage_db):
             os.remove(storage_db)
         self.db_connection = sqlite3.connect(storage_db)
@@ -76,7 +73,7 @@ class ExpertFinding(object):
             logging.info("%s: Number of papers (filtered) with abstract: %d" % (os.path.basename(input_f), sum(1 for p in papers if legit_abstract(p.abstract))))
             logging.info("%s: Number of papers (filtered) with DOI but no abstract %d" % (os.path.basename(input_f), sum(1 for p in papers if not legit_abstract(p.abstract) and p.doi)))
 
-        paper_id = self.next_paper_id()
+        paper_id = self._next_paper_id()
         for p in papers:
             self._add_author(p.author_id, p.name, p.institution)
             self.papers_count[p.author_id] += 1
@@ -170,33 +167,24 @@ class ExpertFinding(object):
     def _add_entities(self, author_id, paper_id, year, entities):
         self.db.executemany('INSERT INTO entities VALUES (?,?,?,?,?)', ((entity, author_id, paper_id, year, rho) for entity, rho in entities))
 
-    def next_paper_id(self):
+    def _next_paper_id(self):
         return self.db.execute('SELECT IFNULL(MAX(paper_id), -1) FROM entities').fetchall()[0][0] + 1
 
     def _add_author(self, author_id, name, institution):
         self.db.execute('INSERT OR IGNORE INTO authors VALUES (?,?,?)', (author_id, name, institution))
 
+    def papers_count(self):
+        return self.db.execute('''
+            SELECT author_id, COUNT(DISTINCT(paper_id))
+            FROM "entities"
+            GROUP BY author_id''').fetchall()
+
     def print_abstract_quantiles(self):
-        print "number of abstracts: {}".format(sum(self.abstract_count.values()))
-        quantiles = stats.mstats.mquantiles([self.abstract_count[a_id] for a_id in self.abstract_count], prob=[n / 10 for n in range(10)])
+        papers_count = [p[1] for p in self.papers_count()]
+        print "number of abstracts: {}".format(sum(papers_count))
+        quantiles = stats.mstats.mquantiles(papers_count, prob=[n / 10.0 for n in range(10)])
         print quantiles
         for i in range(len(quantiles)):
             begin = int(quantiles[i])
-            end = int(quantiles[i + 1]) - 1 if i < len(quantiles) - 1 else max(self.abstract_count.values())
-            print "{} authors have {}-{} papers with abstract".format(sum(1 for a_id in self.abstract_count if begin <= self.abstract_count[a_id] <= end), begin, end)
-
-    def print_entity_stats(self):
-        all_entities = Counter(e for entities in self.id_to_entities.values() for e in entities)
-        print "most common entities"
-        for title, freq in all_entities.most_common(100):
-            print title, freq
-        print "number of entities: {}".format(sum(all_entities.values()))
-        print "number of distinct entities: {}".format(len(all_entities))
-
-    def print_authors_stats(self):
-        for name, a_id in self.name_to_id.iteritems():
-            print "{} (ID {}) papers: {} abstracts:{}".format(name, a_id, self.papers_count[a_id], self.abstract_count[a_id])
-
-    def print_author_stats(self, author_name):
-        for title, freq in Counter(self.id_to_entities[self.name_to_id[author_name]]).most_common():
-            print title, freq
+            end = int(quantiles[i + 1]) - 1 if i < len(quantiles) - 1 else max(papers_count)
+            print "{} authors have {}-{} papers with abstract".format(sum(1 for c in papers_count if begin <= c <= end), begin, end)
