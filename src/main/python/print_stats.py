b04c561b9c71606ff49ca516e894ab4cf5021c04
Marco Cornolti <marco@macor.di.unipi.it>
2017-01-18 12:19:44 +0100

    Added stats main

---
+++ src/main/python/print_stats.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python2.7
+# encoding: utf-8
+
+from __future__ import division
+
+from argparse import ArgumentParser
+import logging
+import sys
+from expertfinding import ExpertFinding
+import expertfinding
+from collections import Counter
+
+
+def main():
+    '''Command line options.'''
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--storage_db", required=True, action="store", help="Storage DB file")
+    args = parser.parse_args()
+
+    exf = ExpertFinding(args.storage_db, False)
+    exf.print_abstract_quantiles()
+    return 0
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(main())
